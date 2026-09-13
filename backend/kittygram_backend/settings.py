7e082f73256efbb54a707148af8b213756ce3eba
Ubuntu <ubuntu@fv4g6338lgi56rraajc6.auto.internal>
2026-09-13 12:28:41 +0000

    second commit

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,9 +1,11 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-test-key-for-ci-only")
 
 DEBUG = False
 
